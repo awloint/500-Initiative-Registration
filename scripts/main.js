b47bac0b914392c6782d0dd1c62c8b6cd9a0b669
Benson Imoh,ST <benson@stbensonimoh.com>
2019-10-03 18:41:20 +0100

    Edit Main Javascript file

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,92 @@
+'use strict'
+google.maps.event.addDomListener(window, 'load', initialize)
+function initialize () {
+  var input = document.getElementById('location')
+  var autocomplete = new google.maps.places.Autocomplete(input)
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-    const elems = document.querySelectorAll('.datepicker');
-    const instances = M.Datepicker.init(elems,);
-});
+  const date = new Date()
+  const year = date.getFullYear()
+  const yearContent = document.querySelector('#year')
+  yearContent.innerHTML = year
+
+  // Capitalize first letter of First Name and Last Name
+  document.querySelector('#firstName').onchange = e => {
+    let val = document.querySelector('#firstName').value
+    RegExp = /\b[a-z]/g
+
+    val = val.charAt(0).toUpperCase() + val.substr(1)
+  }
+
+  document.querySelector('#lastName').onchange = e => {
+    let val = document.querySelector('#lastName').value
+    RegExp = /\b[a-z]/g
+
+    val = val.charAt(0).toUpperCase() + val.substr(1)
+  }
+
+  document.querySelector('#email').onchange = e => {
+    let val = document.querySelector('#email').value
+    RegExp = /\b[a-z]/g
+
+    val = val.toLowerCase()
+  }
+
+  // Add intl-tel-input
+  window.intlTelInputGlobals.loadUtils('scripts/utils.js')
+  var input = document.querySelector('#phone')
+  window.intlTelInput(input, {
+    initialCountry: 'ng',
+    separateDialCode: true,
+    hiddenInput: 'full_phone',
+    utilsScript: 'scripts/utils.js'
+  })
+
+  const elems = document.querySelectorAll('select')
+  const instances = M.FormSelect.init(elems)
+  const count = document.querySelectorAll('textarea')
+  M.CharacterCounter.init(count)
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const elems = document.querySelectorAll('.datepicker')
+    const instances = M.Datepicker.init(elems)
+  })
+
+  //   Submit the form
+  const form = document.querySelector('form')
+
+  // On Form Submit
+  form.addEventListener('submit', e => {
+    // Check to see if form has validation errors
+    if (form.checkValidity() === false) {
+      e.preventDefault()
+      e.stopPropagation()
+    }
+
+    // If form doesn't have validation errors
+    if (form.checkValidity() === true) {
+      e.preventDefault()
+
+      // change the button color and add the loading class
+      //   document.querySelector('button').classList.remove('btn-danger')
+      document.querySelector('button').classList.add('btn-primary')
+      document.querySelector('button').innerHTML =
+        'Loading <span class="spinner"><i class="fa fa-spinner fa-spin"></i></span>'
+
+      // Grab the form data
+      const formdata = new FormData(form)
+
+      // send it for processing
+      fetch('scripts/processor.php', {
+        method: 'post',
+        body: formdata
+      })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(err => {
+          console.log('The request failed', err)
+        })
+    }
+  })
+})
