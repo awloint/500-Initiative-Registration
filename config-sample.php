27819f81a9cfcc63f489d43a301824b34b0f8e0b
Benson Imoh,ST <benson@stbensonimoh.com>
2019-10-15 01:46:09 +0100

    Add Sample Configuration file

---
+++ config-sample.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * This is the configuration file
+ *
+ * PHP version 7.2
+ *
+ * @category Configuration
+ * @package  Configuration_File
+ * @author   Benson Imoh <benson@stbensonimoh.com>
+ * @license  https://opensource.org/licenses/MIT MIT
+ * @link     https://opensource.org/licenses/MIT
+ */
+
+return [
+    'database' => [
+        'host'      =>  '',
+        'name'      =>  '',
+        'user'      =>  '',
+        'password'  =>  ''
+    ],
+    'smstoken' => '',
+    'ravepay'  => [
+        'public_key' => '',
+        'secret_key' => ''
+    ],
+    'mailgun'   => [
+        'apikey'    =>  '',
+        'domain'    =>  '',
+        'list_alias' =>  ''
+    ]
+    ];
